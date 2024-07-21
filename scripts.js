7cb9521d26eda37435127f180df772d24b4861a6
SeanCrofut <sean.crofut@gmail.com>
2024-07-20 21:27:14 -0700

    added intro page

--- scripts.js
+++ scripts.js
@@ -14,11 +14,11 @@ document.addEventListener('DOMContentLoaded', async () => {
             };
         });
         console.log(data); 
-        document.getElementById('data-status').innerText = 'Data loaded successfully.';
+        document.getElementById('scene0').innerText = 'Data loaded successfully.';
 
     } catch (error) {
         console.error('Error loading data', error);
-        document.getElementById('data-status').innerText = 'Failed to load data.';
+        document.getElementById('scene0').innerText = 'Failed to load data.';
     }
 
     // Handle scene navigation
